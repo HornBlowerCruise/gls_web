ba65cc6450a982a41f478d9d211257934d8e40b2
HornBlowerCruise <immanuel0688@gmail.com>
2022-12-29 21:00:19 +0900

    update

--- src/pages/Topmenu01/Submenu01.js
+++ src/pages/Topmenu01/Submenu01.js
@@ -6,6 +6,11 @@ const Submenu01 = () => {
     <div>
       <div className='imgContainer'>
           <img width='100%' src='https://mblogthumb-phinf.pstatic.net/MjAxOTEyMTJfMjI3/MDAxNTc2MTQwMTI3MDQz.BH9CTAPX29-JwHo7tubyLgj79pa_AlukXsEkHS7bZUQg.0cwrlsegcy5RmK5I1bUNMydz555ZTkhCnWE-WJcq6dMg.JPEG.hanbit_goodnews/1576140125987.jpg?type=w800'></img>
+          <div submenuContainer>
+              <item>인사말</item>
+              <item>인사말</item>
+              <item>인사말</item>
+          </div>
       </div>
       <div className='passage'>
         예시 글
